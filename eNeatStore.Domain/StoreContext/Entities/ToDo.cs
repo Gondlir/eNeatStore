059198111087f95ba7b4e9b25574a2f587eb90c7
Gondlir <kaoeskt@hotmail.com>
2021-03-22 06:16:33 -0300

    script to do para ajudar a saber o que fazer

---
+++ eNeatStore.Domain/StoreContext/Entities/ToDo.cs
@@ -0,0 +1,11 @@
+namespace eNeatStore.Domain.StoreContext.Entities
+{
+    public class ToDo
+    {
+        //Criar entidades
+        //Definir função das entidades 
+        //Preencher as regras de negócio necessárias para as entidades
+
+
+    }
+}
