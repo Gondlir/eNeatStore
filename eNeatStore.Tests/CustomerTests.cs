1637d0c4975d029ebf7d038947cc54d9f38b46ba
Gondlir <kaoeskt@hotmail.com>
2021-03-25 18:53:55 -0300

    changed script name

--- eNeatStore.Tests/CustomerTests.cs
+++ eNeatStore.Tests/CustomerTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace eNeatStore.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class CustomerTests
     {
         [TestMethod]
         public void CreateCustomerSuccedd()
